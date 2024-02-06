2793d9544d9ded3f2a64514df8ce8865483d4c43
Karan Sukhani <76897641+karansukhani@users.noreply.github.com>
2024-02-06 16:41:02 +0530

    Add files via upload

---
+++ lib/login.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerceapp/main.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Satguru Creations",style: TextStyle(color: Colors.blue),),
+              backgroundColor: Colors.black,
+      ),
+      body:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Login",style: Theme.of(context).textTheme.bodyLarge,),
+          TextField(
+            decoration: InputDecoration(
+                labelText: 'Username',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(style: BorderStyle.solid,color: Colors.pink),
+                borderRadius: BorderRadius.circular(11),
+              )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: TextField(
+              obscureText: true,
+              obscuringCharacter: '*',
+              decoration:  InputDecoration(
+                labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(style: BorderStyle.solid,color: Colors.pink),
+                   borderRadius: BorderRadius.circular(11),
+                  ),
+              ),
+            ),
+          ),
+          ElevatedButton(onPressed: () {
+ Navigator.push(context, MaterialPageRoute(builder: (context)=>MyHomePage()));
+          }, child: Text('Login'),
+          ),
+        ],
+      ),
+    );
+  }
+  }
